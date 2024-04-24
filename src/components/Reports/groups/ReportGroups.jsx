c4cda9e96b8e583c50f6e6059a95cbdf520297c5
Bekforever13 <b3kforever@gmail.com>
2024-04-24 15:04:44 +0500

    fix

--- src/components/Reports/groups/ReportGroups.jsx
+++ src/components/Reports/groups/ReportGroups.jsx
@@ -1,24 +1,21 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
 import axiosInstance from "../../../api/axiosInterceptor";
 
 const ReportGroups = ({ selectedGroup, setSelectedGroup }) => {
   const [groups, setGroups] = useState();
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true);
     axiosInstance
       .get("/groups?kindergarten_id=2")
-      .then((res) => setGroups(res.data))
-      .finally(() => setIsLoading(false));
+      .then((res) => setGroups(res.data));
   }, []);
 
   return (
     <div className='flex flex-col gap-5'>
       <div
-        className={`text-xl py-3 px-6 bg-white rounded-3xl border border-solid border-black hover:bg-blue-200 cursor-pointer hover:-translate-y-1 ${
-          selectedGroup.id === 0 ? "bg-blue-300" : ""
+        className={`text-xl py-3 px-6 rounded-3xl border border-solid border-black hover:bg-blue-200 cursor-pointer hover:-translate-y-1 ${
+          selectedGroup.id === 0 ? "bg-blue-300" : "bg-white"
         }`}
         onClick={() => setSelectedGroup(0)}
       >
@@ -27,8 +24,8 @@ const ReportGroups = ({ selectedGroup, setSelectedGroup }) => {
       {groups?.data?.map((el) => (
         <div
           key={el.id}
-          className={`text-xl py-3 px-6 bg-white rounded-3xl border border-solid border-black hover:bg-blue-400 cursor-pointer hover:-translate-y-1 ${
-            selectedGroup.id === el.id ? "bg-blue-500 text-white" : ""
+          className={`text-xl py-3 px-6 rounded-3xl border border-solid border-black hover:bg-blue-400 cursor-pointer hover:-translate-y-1 ${
+            +selectedGroup?.id === +el?.id ? "bg-blue-500 text-white" : "bg-white text-black"
           }`}
           onClick={() => setSelectedGroup(el)}
         >
